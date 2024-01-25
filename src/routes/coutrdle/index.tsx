b7e20ed0077859634c5ca10d7f01d8fff1be2c9e
matej-kotrba <matejkotrba.skola@gmail.com>
2024-01-25 19:59:01 +0100

    Some basic functionality

--- src/routes/coutrdle/index.tsx
+++ src/routes/coutrdle/index.tsx
@@ -42,11 +42,13 @@ export default component$(() => {
     filteredCountries: Country[];
     countryToGuess: Country | undefined;
     showedClues: (keyof Country)[];
+    guessedCountries: Country[];
   }>({
     countries: [],
     countryToGuess: undefined,
     showedClues: [],
     filteredCountries: [],
+    guessedCountries: [],
   });
 
   const setNewCountryToGuess = $((excludedCountryCommonName: string) => {
@@ -60,7 +62,7 @@ export default component$(() => {
     countriesStore.countryToGuess = randomCountry;
   });
 
-  const showNextClue = () => {
+  const showNextClue = $(() => {
     const cluesInOrder: (keyof Country)[] = [
       "area",
       "population",
@@ -78,10 +80,15 @@ export default component$(() => {
       return;
     }
     shCl.push(cluesInOrder[shCl.length]);
-  };
+  });
 
   const filterCountryList = $((search: string) => {
+    const guessedCountriesNames = countriesStore.guessedCountries.map(
+      (country) => country.name.common
+    );
+
     countriesStore.filteredCountries = countriesStore.countries
+      .filter((country) => !guessedCountriesNames.includes(country.name.common))
       .filter((country) =>
         country.name.common.toLowerCase().includes(search.toLowerCase())
       )
@@ -90,11 +97,49 @@ export default component$(() => {
       });
   });
 
+  const isCountryGuessed = $((country: Country) => {
+    return (
+      country.name.official === countriesStore.countryToGuess?.name.official
+    );
+  });
+
+  const guessCountry = $(async (query: string) => {
+    const usedCountriesNames = countriesStore.guessedCountries.map(
+      (country) => country.name.common
+    );
+
+    console.log(usedCountriesNames);
+
+    const country = countriesStore.countries
+      .filter((country) => !usedCountriesNames.includes(country.name.common))
+      .find((item) =>
+        item.name.common.toLowerCase().includes(query.toLowerCase())
+      );
+
+    console.log(country?.name.common);
+
+    if (
+      !country ||
+      countriesStore.guessedCountries.find(
+        (item) => item.name.official === country.name.official
+      )
+    )
+      return;
+
+    countriesStore.guessedCountries.push(country);
+    const result = await isCountryGuessed(country);
+    if (result) {
+      alert("You guessed the country!");
+    } else {
+      showNextClue();
+    }
+  });
+
   useTask$(async ({ cleanup }) => {
     const abortController = new AbortController();
     cleanup(() => abortController.abort("cleanup"));
     const countries = await fetch(
-      "https://restcountries.com/v3.1/all?fields=name,flags,region,subregion,population,capitals,currencies,independent,landlocked,area,capital,languages,flag",
+      "https://restcountries.com/v3.1/all?fields=name,flags,region,subregion,population,capitals,currencies,independent,landlocked,area,capital,languages,flag,borders",
       {
         signal: abortController.signal,
       }
@@ -102,7 +147,9 @@ export default component$(() => {
 
     const data = await countries.json();
 
-    countriesStore.countries = data as Country[];
+    countriesStore.countries = (data as Country[]).sort((a, b) => {
+      return a.name.common.localeCompare(b.name.common);
+    });
     filterCountryList("");
     setNewCountryToGuess("");
   });
@@ -112,11 +159,24 @@ export default component$(() => {
       <h3 class="text-center text-4xl uppercase font-bold tracking-widest mb-12">
         Countrdle
       </h3>
-      <div class="flex justify-center">
+      <div class="flex flex-col items-center max-w-96 mx-auto">
         <CountryGuessArea
           countryFilteredList={countriesStore.filteredCountries}
           filterSearchCountries={filterCountryList}
+          onSubmit={guessCountry}
         />
+        <div class="flex flex-col gap-1 w-full mt-2">
+          {countriesStore.guessedCountries.map((country) => {
+            return (
+              <span
+                key={country.name.common}
+                class="inline-block w-full text-lg py-2 bg-slate-100 rounded-md text-center"
+              >
+                {country.flag} {country.name.common}
+              </span>
+            );
+          })}
+        </div>
       </div>
       <div>
         <span>The size of the country is: </span>
@@ -204,29 +264,53 @@ export default component$(() => {
 type CountryGuessAreaProps = {
   countryFilteredList: Country[];
   filterSearchCountries: QRL<(search: string) => void>;
+  onSubmit: QRL<(query: string) => void>;
 };
 
 export const CountryGuessArea = component$(
-  ({ countryFilteredList, filterSearchCountries }: CountryGuessAreaProps) => {
+  ({
+    countryFilteredList,
+    filterSearchCountries,
+    onSubmit,
+  }: CountryGuessAreaProps) => {
     const searchQuery = useSignal("");
+    const hideDropdown = useSignal(true);
 
     return (
-      <div class="relative group">
-        <div class="flex gap-1 relative">
+      <div class="relative group w-full">
+        <div
+          class="flex gap-1 relative"
+          onClick$={() => {
+            hideDropdown.value = false;
+          }}
+        >
           <input
             type="text"
             value={searchQuery.value}
+            onKeyDown$={(e) => {
+              if (e.key === "Enter") {
+                hideDropdown.value = true;
+                onSubmit(searchQuery.value);
+              }
+            }}
             onInput$={(e) => {
+              hideDropdown.value = false;
               const value = (e.target as HTMLInputElement).value;
               if (typeof value === "string") {
+                searchQuery.value = value;
                 filterSearchCountries(value);
               }
             }}
-            class="min-w-80 p-2 border-[1px] shadow-md border-solid border-slate-700 bg-slate-100 rounded-md outline-none focus:border-blue-400 duration-100"
+            class="w-full p-2 border-[1px] shadow-md border-solid border-slate-700 bg-slate-100 rounded-md outline-none focus:border-blue-400 duration-100"
           />
           <button
             type="button"
             class="rounded-lg p-2 bg-slate-200 hover:bg-blue-200 duration-100 h-full grid place-content-center border-[1px] border-black border-solid"
+            onClick$={(e) => {
+              hideDropdown.value = true;
+              onSubmit(searchQuery.value);
+              e.stopPropagation();
+            }}
           >
             <iconify-icon
               icon="mynaui:send"
@@ -235,9 +319,9 @@ export const CountryGuessArea = component$(
           </button>
         </div>
         <div
-          class="flex flex-col absolute left-1/2 top-12 -translate-x-1/2 w-full duration-150 opacity-0
-         pointer-events-none group-focus-within:opacity-100 group-focus-within:pointer-events-auto
-         bg-slate-50 rounded-md"
+          class={`flex flex-col absolute left-1/2 top-12 -translate-x-1/2 w-full duration-150 opacity-0
+         pointer-events-none ${hideDropdown.value ? "" : "group-focus-within:opacity-100 group-focus-within:pointer-events-auto"}
+         bg-slate-50 rounded-md`}
         >
           {[...countryFilteredList].splice(0, 8).map((country) => {
             return (
